98bee9ac7b81336ee36277070467eed56b4ec413
Oleksandr Rad <clavigo365@gmail.com>
2024-11-16 18:21:34 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const ul = document.querySelector('ul');
+const target = document.getElementById('largeImg');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const link = e.target.closest('.list-item__link');
+  const href = new URL(link.getAttribute('href'), window.location.origin).href;
+
+  target.setAttribute('src', href);
+});
