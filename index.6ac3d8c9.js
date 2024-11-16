0f7a03e6f22aa083feddbdb049efa3f04d495234
Oleksandr Rad <clavigo365@gmail.com>
2024-11-16 18:26:27 +0200

    make build

---
+++ index.6ac3d8c9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("ul"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link").getAttribute("href");t.setAttribute("src",r)});
+//# sourceMappingURL=index.6ac3d8c9.js.map
